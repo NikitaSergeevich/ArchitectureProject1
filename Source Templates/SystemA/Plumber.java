010f26b277a7ace22bb4de196db7da440f0a8585
Suharik333 <@Sakhankov>
2016-02-14 20:39:19 +0300

    System A ready

---
+++ Source Templates/SystemA/Plumber.java
@@ -0,0 +1,62 @@
+package SystemA;
+
+import CommonFiles.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Plumber {
+
+    public static void main(String argv[]) throws InterruptedException {
+
+        /****************************************************************************
+         * Here we instantiate three filters.
+         ****************************************************************************/
+
+        SourceFilter sourceSubSetA = new SourceFilter("FlightData.dat");
+        SinkFilter sinkOutputA = new SinkFilter("OutputA.dat");
+
+        ColumnRemover columnRemover = new ColumnRemover(getColumnsToShowInOutputFile());
+
+        FrameValuesConverter converterTemperatureAndAltitude = new FrameValuesConverter(createConverters());
+
+        /****************************************************************************
+         * Here we connect the filters starting with the sink filter (filter1) which we connect to
+         * filter2 the middle filter. Then we connect filter2 to the source filter (filter3). You
+         * must connect filters starting with the sink filter and working your way back to the
+         * source as shown here.
+         ****************************************************************************/
+
+        sinkOutputA.connect(converterTemperatureAndAltitude);
+        converterTemperatureAndAltitude.connect(columnRemover);
+        columnRemover.connect(sourceSubSetA);
+
+        /****************************************************************************
+         * Here we start the filters up.
+         ****************************************************************************/
+
+        sourceSubSetA.start();
+        Thread.sleep(90);
+        columnRemover.start();
+        Thread.sleep(90);
+        converterTemperatureAndAltitude.start();
+        Thread.sleep(90);
+        sinkOutputA.start();
+
+    } // main
+
+    private static List<Integer> getColumnsToShowInOutputFile() {
+        List<Integer> columnsToShowInOutput = new ArrayList<>();
+        columnsToShowInOutput.add(Frame.TEMPERATURE);
+        columnsToShowInOutput.add(Frame.ALTITUDE);
+        columnsToShowInOutput.add(Frame.PRESSURE);
+        return columnsToShowInOutput;
+    }
+
+    private static List<Converter> createConverters() {
+        List<Converter> converters = new ArrayList<Converter>();
+        converters.add(new TemperatureConv());
+        converters.add(new AltitudeConv());
+        return converters;
+    }
+} // PlumberTemplate
